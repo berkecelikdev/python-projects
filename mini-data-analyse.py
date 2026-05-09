df306b9091bc5125c8db9b628d2168960f1c025a
berkecelikdev <berkecelik1221@gmail.com>
2026-05-10 01:09:55 +0300

    Create mini-data-analyse.py

---
+++ mini-data-analyse.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+students = pd.read_csv("/Users/berkecelik/Documents/python test/students.csv")
+
+print(students.head())
+
+print(students.describe(include="all"))
+
+print(students.math_grade.mean())
+
+print(students.math_grade.median())
+
+print(students.math_grade.mode()[0])
+
+print(students.math_grade.max() - students.math_grade.min())
+
+print(students.math_grade.std())
+
+sns.histplot(x = "math_grade", data = students)
+plt.show()
+plt.clf()
+
+sns.boxplot(x = "math_grade", data = students)
+plt.show()
+plt.clf()
+
+print(students.Mjob.value_counts())
+
+print(students.Mjob.value_counts(normalize=True))
+
+sns.countplot(x = "Mjob", data = students)
+plt.show()
+plt.clf()
+
+students.Mjob.value_counts().plot.pie()
+plt.show()
+plt.clf()
